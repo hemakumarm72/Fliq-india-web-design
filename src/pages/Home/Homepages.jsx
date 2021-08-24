b3593a92892606f4df2b7169d723580dff0fff6c
unknown <hemakumarm72@gmail.com>
2021-08-24 19:33:57 +0530

    meta data add

--- src/pages/Home/Homepages.jsx
+++ src/pages/Home/Homepages.jsx
@@ -6,6 +6,10 @@ function Homepages() {
     return (
         <>
             <h1>homepages</h1>
+            <img
+                src="https://drive.google.com/uc?export=view&id=1jaksyLRlytnjBPdyYKXCl8_f1IUlD4if"
+                alt=""
+            />
         </>
     );
 }
